91d2aa4517b0ae7fbf80138bed78dd9ac057ed23
MathiasMadsen <m1999@live.dk>
2020-12-22 09:41:56 +0100

    Tilføjet hjælpe metode i SimpleTest kaldet linjeskift

--- src/sample/SimpleTest.java
+++ src/sample/SimpleTest.java
@@ -3,5 +3,14 @@ package sample;
 public class SimpleTest {
     public static void main(String[] args) {
         System.out.println("Velkommen til min opskrifts.addlikations-dummy");
+        linjeskift();
+        linjeskift();
+        System.out.println("********************");
+        System.out.println("Indhold i kogebogen");
+        System.out.println("********************");
+    }
+
+    public static void linjeskift(){
+        System.out.println("     ");
     }
 }
