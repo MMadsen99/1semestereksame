31a0e7ae2195c07c44de6a665d8d30c6f616dbcf
MathiasMadsen <m1999@live.dk>
2020-12-29 20:15:05 +0100

    Imageview glasur og citronte tilføjet

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Controller {
@@ -22,8 +23,12 @@ public class Controller {
     @FXML
     private ImageView BilledeImageView;
 
+    Image glasur = new Image("/Image/glasur.jpg");
+    Image citron = new Image("/Image/citron.jpg");
 
     public void citronTe(){
+
+        BilledeImageView.setImage(citron);
         FremTextArea.clear();
         IngTextArea.clear();
         Opskrift opskrift = new Opskrift("Citron te","skær en citron ud i både og lig dem i kogende vand, lad citronen ligge i vandt i 10 min",1,10);
@@ -32,11 +37,16 @@ public class Controller {
         IngTextArea.appendText("- Kogende vand \n- Citron");
     }
     public void glasur(){
+        BilledeImageView.setImage(glasur);
         FremTextArea.clear();
         IngTextArea.clear();
         Opskrift glasurOpskrift = new Opskrift("Glasur","Bland vand og flormelis indtil det får den konsistens du ville have din glasur i",1,5);
         FremTextArea.appendText(glasurOpskrift.fremgangsmaade);
 
-       IngTextArea.appendText("- Vand \n- Flormelis");
+        for (int i = 0; i < glasurOpskrift.ingredienser.size(); i++) {
+
+
+
+        }
     }
 }
